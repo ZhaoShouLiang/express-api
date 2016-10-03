2b424fbf9aea5f68c553ede07b520ed2b617f22a
ZhaoShouLiang <m15133573182@163.com>
2016-10-03 10:36:51 +0800

    up

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-var express = require('express')
-var app = express()
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/zsldb');
 
-app.get('/123', function (req, res) {
-    console.log('123')
-  res.send('Hello 123')
-})
-app.get('/zsl', function (req, res) {
-  console.log('zsl')
-  res.send('Hello zsl')
-})
+var db = mongoose.connection;
 
-app.listen(3000,function(){
-  console.log('running on port 3000... vist http://localhost:3000')
+db.once('open', function(){
+  console.log('success')
+  var perSchema = mongoose.Schema({
+      name: String,
+      pasward:Number,
+      age:Number
+    });
+  var Person = mongoose.model('person', perSchema);
+    // var ZSL = new Person({ name: 'ZSL' ,pasward:925925,age:22});增加
+    // console.log(ZSL.name);
+    // ZSL.save();
+    // ZSL.pasward = 123456;
+    // ZSL.save();
+  Person.findById({_id : "57ecd029067cc3271aa483ee"},function(err,person){
+      // person.name = "zzzzz"
+      // person.pasward = "321654"
+      // person.save(function(err){
+      // console.log('更新了!')
+      person.remove(function(err){
+        console.log('删除!')
+        Person.find().exec(function(err,people){
+        // 异步执行，将查找的结果打印出来
+        console.log(people);
+        });
+      });
+  });
+  console.log('我先出来')
 })
